771d43b152c1ea7a94e4ac2c73ce8e92add6e4a6
Gaurav Sharma <gauraxvx0410@gmail.com>
2023-10-23 16:16:59 +0530

    aas

--- Backend/Routes/traveller.route.js
+++ Backend/Routes/traveller.route.js
@@ -4,8 +4,8 @@ const {Router} = require("express");
 const travellerController = Router();
 
 travellerController.post("/travellerdetails",async(req,res)=>{
-    const {firstName,lastName,email,phoneNumber,dateOfBirth,gender,nationality} = req.body;
-await TravellerModel.create( {firstName,lastName,email,phoneNumber,dateOfBirth,gender,nationality});
+    const {title,first_N,last_N,email,phone,day,month,year,gender,nationality,c_holder,exp_day,c_number,cvv} = req.body;
+await TravellerModel.create(  {title,first_N,last_N,email,phone,day,month,year,gender,nationality,c_holder,exp_day,c_number,cvv});
 
 res.send("Thankyou for details")
 })
--- Backend/model/traveller.model.js
+++ Backend/model/traveller.model.js
@@ -1,13 +1,20 @@
 const mongoose = require("mongoose");
 
 const travellerSchema = mongoose.Schema({
-    firstName: {type: String,required: true},
-    lastName: {type: String,required: true},
+    title: {type: String,required: true},
+    first_N: {type: String,required: true},
+    last_N: {type: String,required: true},
     email: {type: String,required: true},
-    phoneNumber: {type: Number,required: true},
-    dateOfBirth: {type: String,required: true},
+    phone: {type: Number,required: true},
+    day: {type: String,required: true},
+    month: {type: String,required: true},
+    year: {type: String,required: true},
     gender: {type: String,required: true},
     nationality: {type: String,required: true},
+    c_holder: {type: String,required: true},
+    exp_day: {type: String,required: true},
+    c_number: {type: String,required: true},
+    cvv: {type: String,required: true},
 })
 
 const TravellerModel = mongoose.model("traveller detail", travellerSchema);
